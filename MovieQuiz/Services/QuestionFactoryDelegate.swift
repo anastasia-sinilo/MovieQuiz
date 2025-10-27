92495ab7b22a83b07f3452dab0544ce1a8549fa4
Anastasia <anastasia.m.sinilo@gmail.com>
2025-10-28 03:21:25 +0400

    Добавлена возможность загрузки данных из сети

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -1,4 +1,5 @@
 protocol QuestionFactoryDelegate: AnyObject{
     func didReceiveNextQuestion(question: QuizQuestion?)
-    
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error)
 }
